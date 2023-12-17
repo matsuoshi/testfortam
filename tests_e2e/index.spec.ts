4ea2300e18d607450bcbb4548c6b27ff4b6355ce
matsuoshi <matsuoshi@gmail.com>
2023-12-17 10:54:15 +0900

    add increment and decrement test

--- tests_e2e/index.spec.ts
+++ tests_e2e/index.spec.ts
@@ -1,13 +1,25 @@
 import { test, expect } from '@playwright/test'
 
 test('should navigate to the index page', async ({ page }) => {
-  const response = await page.goto('http://localhost:3000/')
+  await page.goto('http://localhost:3000/')
   await expect(page.locator('h1')).toContainText('counter')
-  expect(response?.status()).toBe(200)
 })
 
 test('should navigate to the 404 page', async ({ page }) => {
   const response = await page.goto('http://localhost:3000/404')
   await expect(page.locator('h1')).toContainText('404')
   expect(response?.status()).toBe(404)
+})
+
+test('increment and decrement', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+  await expect(page.locator('h1')).toContainText('10')
+
+  await page.getByRole('button', { name: 'increment' }).click();
+  await expect(page.locator('h1')).toContainText('11')
+
+  await page.getByRole('button', { name: 'decrement' }).click();
+  await page.getByRole('button', { name: 'decrement' }).click();
+  await page.getByRole('button', { name: 'decrement' }).click();
+  await expect(page.locator('h1')).toContainText('8')
 })
